07b57da5e42fa91064c2af39e3fb09ebe2e409ee
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-02-03 21:53:47 -0400

    script.js

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ const Choices = {
     SCISSORS: "scissors"
 };
 
-let buttons = document.querySelectorAll('button');
-const result = document.querySelector('.result');
-const yourResult = document.querySelector('.your-score');
-const machineResult = document.querySelector('.machine-score');
+let buttons = document.querySelectorAll('button')
+const result = document.querySelector('.result')
+const yourResult = document.querySelector('.your-score')
+const machineResult = document.querySelector('.machine-score')
 
 buttons.forEach(button => {
-    button.addEventListener('click', () => play(button.id));
+    button.addEventListener('click', () => play(button.id))
 });
 
-let yourScore = 0;
-let machineScore = 0;
+let yourScore = 0
+let machineScore = 0
 
 let play = playerChoice => {
-    let choices = Object.values(Choices);
-    let machineChoice = choices[Math.floor(Math.random() * choices.length)];
+    let choices = Object.values(Choices)
+    let machineChoice = choices[Math.floor(Math.random() * choices.length)]
 
     if (playerChoice === machineChoice) {
         result.innerHTML = `Resultado da partida:
@@ -27,7 +27,7 @@ let play = playerChoice => {
         <br>
         Sua escolha: ${playerChoice}
         <br>
-        Escolha da Alexa: ${machineChoice}`;
+        Escolha da Alexa: ${machineChoice}`
     } else if (
         (playerChoice === Choices.ROCK && machineChoice === Choices.SCISSORS) ||
         (playerChoice === Choices.PAPER && machineChoice === Choices.ROCK) ||
@@ -39,10 +39,10 @@ let play = playerChoice => {
         <br>
         Sua escolha: ${playerChoice}
         <br>
-        Escolha da Alexa: ${machineChoice}`;
+        Escolha da Alexa: ${machineChoice}`
 
         yourScore++;
-        yourResult.innerHTML = `Sua pontuação: <span>${yourScore}</span>`;
+        yourResult.innerHTML = `Sua pontuação: <span>${yourScore}</span>`
     } else {
         result.innerHTML = `Resultado da partida:
         <br>
@@ -50,9 +50,9 @@ let play = playerChoice => {
         <br>
         Sua escolha: ${playerChoice}
         <br>
-        Escolha da Alexa: ${machineChoice}`;
+        Escolha da Alexa: ${machineChoice}`
 
-        machineScore++;
-        machineResult.innerHTML = `Pontuação da Alexa: <span>${machineScore}</span>`;
+        machineScore++
+        machineResult.innerHTML = `Pontuação da Alexa: <span>${machineScore}</span>`
     }
 };
